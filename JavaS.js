328af04bc9cdf295f07eb549762e6054ee300ad1
Denis <97852360+BarsDenis@users.noreply.github.com>
2022-04-24 16:49:56 +0300

    Update JavaS.js

--- JavaS.js
+++ JavaS.js
@@ -1,17 +1,29 @@
 let photos  = document.querySelectorAll ('.miniPhoto')
-let slides =  Array.from(photos)   
+let slides =  Array.from(photos)
 let photoBtn = document.querySelector('#btn')
+// slides.push(photoBtn)
 let hidePhoto = document.querySelector ('.hidePhoto')
 let activeBtn = document.querySelector('#activeBTn')
 let body = document.querySelector('#body')
 let prevButn = document.querySelector('.prev')
 let nextButn = document.querySelector('.next')
 let cover = document.querySelector('.cover')
+let i = 0
 
 photoBtn.addEventListener ('click', () => {
     document.photoBtn = document.querySelector('#btn')
     activeSlides ()
     } 
+)
+
+slides.forEach(index=> index.addEventListener('click', (event) => {
+i = event.currentTarget.dataset.id
+slides[i].classList.toggle('photo_img'), slides[i].classList.toggle('activeAva')
+prevButn.style.display = 'block'
+nextButn.style.display = 'block'
+document.querySelector('.cover').classList.add('cover_show')
+}
+)
 )
 
 function activeSlides () {
@@ -29,16 +41,16 @@ nextButn.addEventListener('click', () => {
    pCounter(slides)
 })
 
-let i = 0
+
 
 function pCounter (slides) {
    slides[i].classList.toggle('photo_img'), slides[i].classList.toggle('activeAva')
    i++
    if (i >= slides.length ) {
     i = 0 
-    return slides[i].classList.toggle('photo_img'), slides[i].classList.toggle('activeAva')
+    return slides[i].classList.remove('photo_img'), slides[i].classList.toggle('activeAva')
     } else  {
-    return slides[i].classList.toggle('photo_img'), slides[i].classList.toggle('activeAva')
+    return slides[i].classList.remove('photo_img'), slides[i].classList.toggle('activeAva')
   } 
 }
 
